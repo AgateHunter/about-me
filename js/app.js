d97fb0af9bb29ecd316e1f4a85a1965daaa3a920
zachary moore <zach.moore0628@gmail.com>
2023-10-26 18:05:01 -0500

    creating functions

--- js/app.js
+++ js/app.js
@@ -1,23 +1,62 @@
-let topActivity = "Lego";
+"use strict"
 
-let activityGuess = "What is my favorite activity?"
-
+let topActivity = "lego";
+let activityGuess = prompt("What is my favorite activity?");
 let normalizedActivityGuess = activityGuess.toLowerCase();
+let rightAnswers = 0;
+console.log(normalizedActivityGuess);
 
-console.log(activityGuess);
-
-
+function lego() {
 if (normalizedActivityGuess === "lego" ) {
     console.log("Exactly, plus you spelled it right!");
+    
+    
 
-    alert("That's right!" + activityGuess + "Is my favorite activity.");
-
+    alert("That's right ! " + normalizedActivityGuess + " Is my favorite activity.");
+    rightAnswers++;
 }
 
 else {
     
     console.log("Incorrect");
-    alert("That might be fun, but it is not my top activity. Try again please")
+    alert("That might be fun, but it is not my top activity.")
+}
 }
 
-//Testing
+lego();
+
+function beverageChoice(){
+let coffeeGuess = prompt('Do I like coffee?');
+    coffeeGuess = coffeeGuess.toLowerCase();
+
+    if (coffeeGuess === "yes") {
+        alert("Yup, can i have another cup!?"); 
+        console.log(coffeeGuess); 
+        rightAnswers++;
+}   else if (coffeeGuess === "no"){
+        alert("Wrong, I love coffee!")
+} else  {
+    alert("Please guess yes or no.");
+}
+}
+
+beverageChoice();
+  
+function rock(){
+let rockClimbing = "yes";
+let rockGuess = prompt("Do I like rock climbing?")
+    rockGuess= rockGuess.toLowerCase();
+
+    switch (rockGuess) {
+        case "yes":
+        alert("Send it!");
+        rightAnswers++;
+        break;
+
+        case "no":
+            alert("Break out the beta spray!");
+            break;
+    }
+}
+rock();
+   
